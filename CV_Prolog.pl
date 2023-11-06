d4d3ed80c5907b380ee2a68cb97cf6478cd784fd
jholaj <thezmrdus@gmail.com>
2023-11-06 13:04:43 +0100

    Init Prolog file

---
+++ CV_Prolog.pl
@@ -0,0 +1,22 @@
+% Ve VSC => Load document => cislice(0). => true. // cislice(a). => false.
+% cislice(objekt, ktery bude cislici).
+% AND operator => ,
+% OR operator => ;
+% NOT operator => \+
+% Deleni => /
+% Celociselne deleni => //
+% Mensi\Vetsi nebo rovno => >= // =<
+% Promenne => Velke pismena // cislice(C).
+
+% Program.
+
+cislice(0).
+cislice(1).
+cislice(2).
+cislice(3).
+cislice(4).
+cislice(5).
+cislice(6).
+cislice(7).
+cislice(8).
+cislice(9).
